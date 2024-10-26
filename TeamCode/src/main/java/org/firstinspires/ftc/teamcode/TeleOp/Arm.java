6439448bb1efd5539e1fbed192a5abe71485ce3c
Mukul Kedia <71291707+Mukul1127@users.noreply.github.com>
2024-10-26 15:09:47 -0400

    Fully working Arm!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Arm.java
@@ -5,11 +5,12 @@ import androidx.annotation.NonNull;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Arm {
     private static DcMotorEx motorLeft, motorRight;
-    private static Servo servoLeft, servoRight;
-    private static final double SERVO_DEFAULT = 0.35, SERVO_CHANGE = 0.15;
+    public static Servo servoLeft, servoRight;
+    private static final double SERVO_DEFAULT = 0.35, SERVO_CHANGE = 0.1;
 
     public static void init(@NonNull HardwareMap hardwareMap) {
         motorLeft = hardwareMap.get(DcMotorEx.class, "slideMotorLeft");
@@ -17,21 +18,22 @@ public class Arm {
         servoLeft = hardwareMap.get(Servo.class, "slideServoLeft");
         servoRight = hardwareMap.get(Servo.class, "slideServoRight");
 
-        motorLeft.setDirection(DcMotorEx.Direction.FORWARD);
-        motorRight.setDirection(DcMotorEx.Direction.REVERSE);
+        motorLeft.setDirection(DcMotorEx.Direction.REVERSE);
+        motorRight.setDirection(DcMotorEx.Direction.FORWARD);
         servoLeft.setPosition(SERVO_DEFAULT);
-        servoRight.setPosition(-SERVO_DEFAULT);
+        servoRight.setPosition(SERVO_DEFAULT);
     }
 
-    public static void handleInput(float left_stick_y, boolean left_bumper, boolean right_bumper) throws InterruptedException {
+    public static void handleInput(float left_stick_y, boolean left_bumper, boolean right_bumper, Telemetry telemetry) throws InterruptedException {
         motorLeft.setPower(left_stick_y);
         motorRight.setPower(left_stick_y);
-        if (left_bumper && servoLeft.getPosition() > SERVO_DEFAULT) {
+
+        if (left_bumper && servoLeft.getPosition() >= SERVO_DEFAULT - (SERVO_CHANGE / 2)) {
             servoLeft.setPosition(servoLeft.getPosition() - SERVO_CHANGE);
             servoRight.setPosition(servoRight.getPosition() + SERVO_CHANGE);
             Thread.sleep(500);
         }
-        if (right_bumper && servoRight.getPosition() < SERVO_DEFAULT) {
+        if (right_bumper && servoLeft.getPosition() <= SERVO_DEFAULT + (SERVO_CHANGE / 2)) {
             servoLeft.setPosition(servoLeft.getPosition() + SERVO_CHANGE);
             servoRight.setPosition(servoRight.getPosition() - SERVO_CHANGE);
             Thread.sleep(500);
