2652fdbfdf64035c4b0742748acc9f72c626fd99
Mukul Kedia <71291707+Mukul1127@users.noreply.github.com>
2024-11-20 20:15:32 -0500

    Add second claw servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Claw.java
@@ -6,22 +6,25 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Claw {
-    private static Servo servoBottom, servoTop;
-    private static final double SERVO_BOTTOM_MIN = 0, SERVO_BOTTOM_DEFAULT = 0.5, SERVO_BOTTOM_MAX = 1, SERVO_TOP_MIN = 0, SERVO_TOP_MAX = 1;
+    private static Servo servoBottomLeft, servoBottomRight, servoTop;
+    private static final double SERVO_BOTTOM_MIN = 0, SERVO_BOTTOM_DEFAULT = 0.5, SERVO_BOTTOM_MAX = 1, SERVO_TOP_MIN = 0, SERVO_TOP_MAX = 0.15;
 
     public Claw(@NonNull HardwareMap hardwareMap) {
-        servoBottom = hardwareMap.get(Servo.class, "clawServoBottom");
+        servoBottomLeft = hardwareMap.get(Servo.class, "clawServoBottomLeft");
+        servoBottomRight = hardwareMap.get(Servo.class, "clawServoBottomRight");
         servoTop = hardwareMap.get(Servo.class, "clawServoTop");
 
-        servoBottom.setPosition(SERVO_BOTTOM_DEFAULT);
+        servoBottomLeft.setPosition(SERVO_BOTTOM_DEFAULT);
+        servoBottomRight.setPosition(SERVO_BOTTOM_DEFAULT);
         servoTop.setPosition(SERVO_TOP_MIN);
     }
 
     public static void setBottomOffset(double offset) throws InterruptedException {
         double newPosition = SERVO_BOTTOM_DEFAULT + offset;
         if (SERVO_BOTTOM_MIN <= newPosition && newPosition <= SERVO_BOTTOM_MAX) {
-            servoBottom.setPosition(newPosition);
-            Thread.sleep(1000);
+            servoBottomLeft.setPosition(newPosition);
+            servoBottomRight.setPosition(SERVO_BOTTOM_DEFAULT - offset);
+            Thread.sleep(3000);
         }
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Claw.java
@@ -6,15 +6,17 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Claw {
-    private static Servo servoBottom, servoTop;
-    private static final double SERVO_BOTTOM_MIN = 0, SERVO_BOTTOM_MAX = 1, SERVO_BOTTOM_CHANGE = 0.005, SERVO_TOP_MIN = 0, SERVO_TOP_MAX = 0.15;
+    private static Servo servoBottomLeft, servoBottomRight, servoTop;
+    private static final double SERVO_BOTTOM_MIN = 0, SERVO_BOTTOM_DEFAULT = 0.5, SERVO_BOTTOM_MAX = 1, SERVO_BOTTOM_CHANGE = 0.005, SERVO_TOP_MIN = 0, SERVO_TOP_MAX = 0.15;
     private static double servoOffset = 0;
 
     public Claw(@NonNull HardwareMap hardwareMap) {
-        servoBottom = hardwareMap.get(Servo.class, "clawServoBottom");
+        servoBottomLeft = hardwareMap.get(Servo.class, "clawServoBottomLeft");
+        servoBottomRight = hardwareMap.get(Servo.class, "clawServoBottomRight");
         servoTop = hardwareMap.get(Servo.class, "clawServoTop");
 
-        servoBottom.setPosition(SERVO_BOTTOM_MIN + servoOffset);
+        servoBottomLeft.setPosition(SERVO_BOTTOM_DEFAULT);
+        servoBottomRight.setPosition(SERVO_BOTTOM_DEFAULT);
         servoTop.setPosition(SERVO_TOP_MIN);
     }
 
@@ -24,12 +26,13 @@ public class Claw {
             else servoTop.setPosition(SERVO_TOP_MIN);
             Thread.sleep(500);
         }
-        if (dpad_up && SERVO_BOTTOM_MIN + servoOffset > SERVO_BOTTOM_MIN) {
+        if (dpad_up && SERVO_BOTTOM_DEFAULT + servoOffset > SERVO_BOTTOM_MIN) {
             servoOffset -= SERVO_BOTTOM_CHANGE;
         }
-        if (dpad_down && SERVO_BOTTOM_MIN + servoOffset < SERVO_BOTTOM_MAX) {
+        if (dpad_down && SERVO_BOTTOM_DEFAULT + servoOffset < SERVO_BOTTOM_MAX) {
             servoOffset += SERVO_BOTTOM_CHANGE;
         }
-        servoBottom.setPosition(SERVO_BOTTOM_MIN + servoOffset);
+        servoBottomLeft.setPosition(SERVO_BOTTOM_DEFAULT + servoOffset);
+        servoBottomRight.setPosition(SERVO_BOTTOM_DEFAULT - servoOffset);
     }
 }
